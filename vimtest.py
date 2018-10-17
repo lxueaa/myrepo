f8d2fa9da727d06a29e0c8a68322093139500fda
XUE Lanqing <lxueaa@lzcpu1.cse.ust.hk>
2018-10-17 09:29:59 +0800

    add a liniear model in vimtest

--- vimtest.py
+++ vimtest.py
@@ -11,3 +11,39 @@ data_size = 100
 device = torch.device("cuda:0" if torch.cuda.is_avaliable() else "cpu")
 
 class RandomDataset(Datast):
+
+	def __init__(self, size, length):
+		self.len = length
+		self.data = torch.randn(length, size)
+	
+	def __getitem__(self, index):
+		return self.data[index]
+
+	def __len__(self):
+		return self.len
+
+
+rand_loader = DataLoader(dataset=RandomDatast(input_size, data_size), batch_size=batch_size, shuffle = True)
+
+
+class Model(nn.Module):
+
+	def __init__(self, input_size, output_size):
+		super(Model, self).__init__()
+		self.fc = nn.Linear(input_size, output_size)
+	
+	def forward(self, input):
+		output = self.fc(input)
+		print("\tIn Model: input size", input.size(), 
+			"output size", output.size())
+
+		return output
+
+
+model = Model(input_size, output_size)
+if torch.cuda.device_count() > 1:
+	print("Let's use", torch.cuda.device_count(), "GPUs!")
+	model = nn.DataParallel(model)
+
+model.to(device)
+
